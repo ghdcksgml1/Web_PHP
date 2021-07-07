7298179b6ec7af099e472f20194fe792d16a6c24
ghdcksgml1 <ghdcksgml2@naver.com>
2021-07-07 20:36:36 +0900

    2021.07.07

---
+++ 10-1-2_strtoupperLower.php
@@ -0,0 +1,12 @@
+<?php
+    $str = "Hello World";
+    echo "원문 <br/>";
+    echo $str;
+    echo "<br/><br/>";
+
+    echo "대문자로 출력 <br/>";
+    echo strtoupper($str);
+    echo "<br/><br/>";
+    echo "소문자로 출력 <br/>";
+    echo strtolower($str);
+?>
