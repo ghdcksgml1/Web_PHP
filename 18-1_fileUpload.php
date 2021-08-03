df443b8c194534dc7866b8de4842e256491c9c33
ghdcksgml1 <ghdcksgml2@naver.com>
2021-08-04 02:00:56 +0900

    2021.08.04

---
+++ 18-1_fileUpload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>File UpLoad</title>
+</head>
+<body>
+    <h1>파일 업로드</h1>
+    <form name="fileUpLoad" method="post" action="18-2_file.php" enctype="multipart/form-data">
+        <input type="file" name="myImage">
+        <input type="submit" value="파일등록">
+    </form>
+</body>
+</html>
---
+++ 18-2_file.php
@@ -0,0 +1,23 @@
+<?php
+    $myTempFile = $_FILES['myImage']['tmp_name'];
+
+    $fileTypeExtension = explode("/",$_FILES['myImage']['type']);
+
+    $fileType = $fileTypeExtension[0];
+    $extension = $fileTypeExtension[1];
+
+    if($fileType == 'image'){
+        if($extension == 'jpeg' || $extension == 'bmp' || $extension == 'gif' || $extension == 'png'){
+            $myFile = "./{$_FILES['myImage']['name']}";
+            $imageUpload = move_uploaded_file($myTempFile,$myFile);
+
+            if($imageUpload == true){
+                echo "파일이 정상적으로 업로드되었습니다.";
+                echo "<img src='{$myFile}' width='200'/>";
+            }else{
+                echo '파일 업로드에 실패했습니다.';
+            }
+        }
+
+    }
+?>
